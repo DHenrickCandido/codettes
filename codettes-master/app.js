ec68f9e280b97e64f6451108c0a69702ee2bc8b5
Diego Henrick <diegohenrick@Candidos-Macbook-Pro.local>
2024-08-03 17:47:04 -0300

    website

---
+++ codettes-master/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+const app = express();
+const port = 3000;
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '')));
+
+const db = new sqlite3.Database('database/database.db');
+
+db.serialize(() => {
+    db.run("CREATE TABLE IF NOT EXISTS subscribers (id INTEGER PRIMARY KEY, email TEXT NOT NULL)");
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '', 'index.html'));
+});
+
+app.post('/subscribe', (req, res) => {
+    const email = req.body.email;
+
+    db.run("INSERT INTO subscribers (email) VALUES (?)", [email], (err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log(`New subscriber: ${email}`);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
