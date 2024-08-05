23d0d934da746c8bff9ab8cd2c766591fe31ce1f
Diego Henrick <diegohenrick@Candidos-Macbook-Pro.local>
2024-08-04 22:58:56 -0300

    maybe fixed static

--- codettes-master/app.js
+++ codettes-master/app.js
@@ -7,28 +7,28 @@ const port = 3000;
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-const db = new sqlite3.Database('database/database.db');
+// const db = new sqlite3.Database('database/database.db');
 
-db.serialize(() => {
-    db.run("CREATE TABLE IF NOT EXISTS subscribers (id INTEGER PRIMARY KEY, email TEXT NOT NULL)");
-});
+// db.serialize(() => {
+//     db.run("CREATE TABLE IF NOT EXISTS subscribers (id INTEGER PRIMARY KEY, email TEXT NOT NULL)");
+// });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.post('/subscribe', (req, res) => {
-    const email = req.body.email;
+// app.post('/subscribe', (req, res) => {
+//     const email = req.body.email;
 
-    db.run("INSERT INTO subscribers (email) VALUES (?)", [email], (err) => {
-        if (err) {
-            return console.error(err.message);
-        }
-        console.log(`New subscriber: ${email}`);
-    });
-});
+//     db.run("INSERT INTO subscribers (email) VALUES (?)", [email], (err) => {
+//         if (err) {
+//             return console.error(err.message);
+//         }
+//         console.log(`New subscriber: ${email}`);
+//     });
+// });
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
